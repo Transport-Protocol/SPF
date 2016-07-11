ccc661fe88b6075f755aea358aef3b4fcd5e58b6
Philipp Proegel <philipp.proegel@haw-hamburg.de>
2016-07-11 12:02:49 +0200

    rabbitmq node test programm für rpc

---
+++ code/receive.js
@@ -0,0 +1,16 @@
+/**
+ * Created by phili on 11.07.2016.
+ */
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', function(err, conn) {
+    conn.createChannel(function(err, ch) {
+        var q = 'hello';
+
+        ch.assertQueue(q, {durable: false});
+        console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
+        ch.consume(q, function(msg) {
+            console.log(" [x] Received %s", msg.content.toString());
+        }, {noAck: true});
+    });
+});
