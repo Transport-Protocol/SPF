e604f071589df7cde0d6550a31dcf009e4b2d076
Philipp Proegel <philipp.proegel@haw-hamburg.de>
2016-07-15 11:39:44 +0200

    fileupload für rest exemplarisch nachgestellt

--- code/RestTest/TestRestServer.js
+++ code/RestTest/TestRestServer.js
@@ -3,13 +3,16 @@
  */
 // call the packages we need
 var express    = require('express');        // call express
+var fileUpload = require('express-fileupload');
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//reading multipart fileupload
+app.use(fileUpload());
 
 var port = process.env.PORT || 8080;        // set our port
 
@@ -28,6 +31,25 @@ router.route('/dropbox')
     .get(function(req,res) {
        console.log(req.query.file);
        res.json({message: 'Dropbox get route'});
+    });
+
+router.route('/dropbox')
+    .put(function(req,res) {
+        var file;
+
+        if (!req.files) {
+            res.send('No files were uploaded.');
+            return;
+        }
+        //file in form data request has to be named as sampleFile
+        file = req.files.file;
+        //data buffer inside req.files.file.data
+            if (!file) {
+                res.status(500).send('error - wrong form-data name');
+            }
+            else {
+                res.send('File uploaded!');
+            }
     });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
