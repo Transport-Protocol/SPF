c5d12bdc9a1f8eafe17719d93ac3c822813ea27d
philippBB <philipp.proegel@haw-hamburg.de>
2016-07-21 10:21:14 +0200

    logger hinzugefuegt

--- code/SeCoApi/server.js
+++ code/SeCoApi/server.js
@@ -1,10 +1,13 @@
 /**
  * Created by phili on 18.07.2016.
  */
-
+//npm modules
 var express = require('express');
 var fileUpload = require('express-fileupload');
 var bodyParser = require('body-parser');
+var winston = require('winston');
+
+//my modules
 var dropbox = require('./routes/dropbox');
 var owncloud = require('./routes/owncloud');
 var github = require('./routes/github');
@@ -12,6 +15,9 @@ var googleDrive = require('./routes/googleDrive');
 var bitbucket = require('./routes/bitbucket');
 var slack = require('./routes/slack');
 
+
+process.env.LOG_LEVEL = 'info';
+winston.level = process.env.LOG_LEVEL;
 
 var app = express();
 app.use(bodyParser.json());
@@ -25,7 +31,7 @@ var router = express.Router();
 // middleware to use for all requests
 app.use(function (req, res, next) {
     // do logging
-    console.log('***new request***');
+    winston.log('info','***new request***');
     next(); // make sure we go to the next routes and don't stop here
 });
 
@@ -44,4 +50,4 @@ app.use('/api/slack',slack);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Api created at port ' + port);
+winston.log('info','Api created at port: ',port);
