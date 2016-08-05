cd407754c4e9178d6b57dec038a2b6f5a0c4fb52
philippBB <philipp.proegel@haw-hamburg.de>
2016-08-05 11:32:35 +0200

    404 route hinzugefügt

--- code/SeCoApi/secoApiStart.js
+++ code/SeCoApi/secoApiStart.js
@@ -43,30 +43,30 @@ function init() {
     //reading multipart fileupload
     app.use(fileUpload());
     router = express.Router();
-
+    app.use(newRequest);
     registerRoutes();
 }
 
-function middleware() {
-    // middleware to use for all requests
-    app.use(function (req, res, next) {
-        // do logging
-        winston.log('info', '***new request***');
-        next(); // make sure we go to the next routes and don't stop here
-    });
+function newRequest(req, res, next) {
+    winston.log('info', '***new request***');
+    next(); // make sure we go to the next routes and don't stop here
+}
+
+function notFound(req, res, next) {
+    winston.log('info', 'route not found');
+    res.status(404).send('not found');
 }
 
 function start() {
     // START THE SERVER
     // =============================================================================
-    console.log('listen');
     app.listen(nconf.get('httpPort'));
     winston.log('info', 'Api created at port: ', nconf.get('httpPort'));
 }
 
 function main() {
     init();
-    middleware();
+    app.use(notFound); //register as last middleware
     start();
 }
 
