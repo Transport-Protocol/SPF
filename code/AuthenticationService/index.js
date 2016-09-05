15e7700e9fb08313c685a630c9a176c8cb0f926a
philippBB <philipp.proegel@haw-hamburg.de>
2016-09-05 14:01:21 +0200

    oauth2 für github und dropbox implementiert

--- code/AuthenticationService/index.js
+++ code/AuthenticationService/index.js
@@ -5,6 +5,7 @@ var express = require('express'),
     app = express(),
     OAuth2 = require('./oauth2/basicOauth2'),
     fs = require('fs'),
+    winston = require('winston'),
     nconf = require('nconf');
 
 nconf.argv()
@@ -16,7 +17,7 @@ app.get('/', function (req, res) {
 });
 
 app.listen(nconf.get('httpPort'), function () {
-    console.log('authentication service http server listening on port %d!',nconf.get('httpPort'));
+    winston.log('info','authentication service http server listening on port %d!',nconf.get('httpPort'));
 });
 
 
@@ -24,15 +25,9 @@ var path = './oauth2/services';
 var services = fs.readdirSync(path);
 
 for(var i = 0;i<services.length;i++){
-    var oauth2 = new OAuth2(path + '/' +  services[i],function(err){
+    var oauth2 = new OAuth2(app,path + '/' +  services[i],function(err){
        if(err){
-           console.log(err);
-       } else {
-           console.log(oauth2.getAuthorizationURL('philipp'));
-           app.get(oauth2.getRedirectRoute(),function(req,res){
-               console.log('redirect received');
-               res.send('ty');
-           });
+           winston.log('error',err);
        }
     });
 }
--- code/AuthenticationService/oauth2/basicOauth2.js
+++ code/AuthenticationService/oauth2/basicOauth2.js
@@ -12,35 +12,91 @@
  */
 
 var fs = require('fs'),
+    winston = require('winston'),
     request = require('request');
 
 
-function OAuth2(filePath,callback) {
-    fs.readFile(filePath,function(err,file){
-        if(err){
+function OAuth2(expressApp,filePath, callback) {
+    this.config = {};
+    var self = this;
+    fs.readFile(filePath, function (err, file) {
+        if (err) {
             return callback(err);
         }
-        this.config = JSON.parse(file);
+        self.config = JSON.parse(file);
+        winston.log('info',self.getAuthorizationURL('philipp'));
+        _registerHttpCallback(self,expressApp);
         return callback(null);
     });
 }
 
+function _registerHttpCallback(self,expressApp){
+    expressApp.get(self.getRedirectRoute(),function(req,res){
+        winston.log('info','redirect received');
+        res.send('ty');
+        self.getAccessToken(res.req.query.state,res.req.query.code,function(err,token){
+            if(err){
+                winston.log('error',err);
+            } else {
+                winston.log('info',token);
+            }
+        });
+    });
+}
+
 OAuth2.prototype.getAuthorizationURL = function (userName) {
-    var baseUrl = 'https://github.com/login/oauth/authorize?';
-    var clientId = 'client_id=' + config.client_id;
-    var redirect_uri = '&redirect_uri=' + config.redirect_uri;
+    var baseUrl = this.config.auth_url;
+    var clientId = '?client_id=' + this.config.client_id;
+    var redirect_uri = '&redirect_uri=' + this.config.redirect_uri;
     var state = '&state=' + userName;
-    return baseUrl + clientId + redirect_uri + state;
+    var fullUrl = baseUrl + clientId + redirect_uri + state;
+    if(this.config.service === 'dropbox'){
+        fullUrl += '&response_type=code';
+    }
+    return fullUrl;
 };
 
 OAuth2.prototype.getRedirectURI = function () {
-    return config.redirect_uri;
+    return this.config.redirect_uri;
 }
 
 OAuth2.prototype.getRedirectRoute = function () {
-    return config.redirect_route;
+    return this.config.redirect_route;
 }
 
+OAuth2.prototype.getServiceName = function () {
+    return this.config.service;
+}
+
+OAuth2.prototype.getAccessToken = function (user,code,callback) {
+    var options = {
+        method: 'POST',
+        uri: this.config.access_token_url,
+        encoding: null,
+        qs: {
+            client_id: this.config.client_id,
+            redirect_uri: this.config.redirect_uri,
+            client_secret: this.config.client_secret,
+            code: code
+        },
+        json: true
+    };
+    if(this.config.service === 'dropbox'){
+        options.qs.grant_type = 'authorization_code';
+    }
+    request(options, function (err, response, body) {
+        if (err) {
+            winston.log('error', 'application error: ', err);
+            return callback(err);
+        }
+        if (response.statusCode >= 400 && response.statusCode <= 499) {
+            winston.log('error', 'http error: ', err);
+            return callback(new Error(response.statusCode + ': ' + response.statusMessage));
+        }
+        winston.log('info', 'succesfully got request token: %s for user %s',body.access_token,user);
+        return callback(null, body.access_token);
+    });
+}
 
 
 module.exports = OAuth2;
