933fd3b760aa5781f3352129c9f00ec0ed60edad
philippBB <philipp.proegel@haw-hamburg.de>
2016-09-07 10:44:59 +0200

    bitbucket init

---
+++ code/Bitbucket/bitbucket.js
@@ -0,0 +1,127 @@
+/**
+ * Created by PhilippMac on 30.08.16.
+ */
+'use strict';
+
+var request = require('request'),
+    winston = require('winston');
+
+
+
+/**
+ * Retrieves all Repos from user
+ * @param auth containing username and password
+ * @param callback err,dirs
+ */
+function getRepositories (auth,callback) {
+    var url = 'https://api.github.com/user/repos';
+    var options = {
+        method: 'GET',
+        uri: url,
+        auth: {
+            user:auth.username,
+            pass:auth.password
+        },
+        headers:{
+            'User-Agent':auth.username
+        }
+    };
+    request(options, function (err, response, body) {
+        if (err) {
+            winston.log('error','application error: ',err);
+            return callback(err);
+        }
+        if (response.statusCode >= 400 && response.statusCode <= 499) {
+            winston.log('error','http error: ',err);
+            console.log(response.body);
+            return callback(new Error(response.statusCode + ': ' + response.statusMessage  + ' ' + body));
+        }
+        var parsed = _parseRepoListBody(body);
+        winston.log('info','succesfully got repos from github',parsed);
+        return callback(null, parsed);
+    });
+};
+
+function addUserToRepo (auth,repo,userToAdd,callback) {
+    var url = 'https://api.github.com/repos/' + auth.username +  '/' + repo + '/collaborators/' + userToAdd;
+    console.log(url);
+    var options = {
+        method: 'PUT',
+        uri: url,
+        auth: {
+            user:auth.username,
+            pass:auth.password
+        },
+        headers:{
+            'User-Agent': auth.username
+        }
+    };
+    request(options, function (err, response, body) {
+        if (err) {
+            winston.log('error','application error: ',err);
+            return callback(err);
+        }
+        if (response.statusCode >= 400 && response.statusCode <= 499) {
+            winston.log('error','http error: ',err);
+            console.log(response.body);
+            return callback(new Error(response.statusCode + ': ' + response.statusMessage  + ' ' + body));
+        }
+        winston.log('info','succesfully added %s to repo %s',userToAdd,repo);
+        return callback(null, 'ok');
+    });
+};
+
+function getRepoContent (auth, repo, path, callback) {
+    var url = 'https://api.github.com/repos/' + auth.username +  '/' + repo + '/contents/' + path;
+    console.log(url);
+    var options = {
+        method: 'GET',
+        uri: url,
+        auth: {
+            user:auth.username,
+            pass:auth.password
+        },
+        headers:{
+            'User-Agent': auth.username
+        }
+    };
+    request(options, function (err, response, body) {
+        if (err) {
+            winston.log('error','application error: ',err);
+            return callback(err);
+        }
+        if (response.statusCode >= 400 && response.statusCode <= 499) {
+            winston.log('error','http error: ',err);
+            console.log(response.body);
+            return callback(new Error(response.statusCode + ': ' + response.statusMessage  + ' ' + body));
+        }
+        var parsed = _parseRepoContent(body);
+        winston.log('info','content for repo %s: ',repo);
+        return callback(null, parsed);
+    });
+};
+
+
+function _parseRepoListBody(body){
+    var parsed = JSON.parse(body);
+    var result = [];
+    for(var i = 0;i<parsed.length;i++){
+        result[i] = {repo:parsed[i].name};
+    }
+    return result;
+}
+
+function _parseRepoContent(body){
+    var parsed = JSON.parse(body);
+    var result = [];
+    for(var i = 0;i<parsed.length;i++){
+        result[i] = {name:parsed[i].name,tag:parsed[i].type};
+    }
+    return result;
+}
+
+module.exports = {
+    getRepositories : getRepositories,
+    addUserToRepo : addUserToRepo,
+    getRepoFiles : getRepoContent
+};
---
+++ code/Bitbucket/grpc/server.js
@@ -0,0 +1,134 @@
+/**
+ * Created by phili on 02.08.2016.
+ */
+'use strict';
+
+var grpc = require('grpc'),
+    winston = require('winston'),
+    connector = require('.././bitbucket');
+
+
+var exports = module.exports = {};
+
+//global var
+var _server;
+
+exports.init = function (serverIp, serverPort) {
+    var versionControlProto = grpc.load('./proto/versionControl.proto').versionControl;
+    _server = new grpc.Server();
+    _server.addProtoService(versionControlProto.VersionControl.service, {
+        getRepositories: getRepositories,
+        getRepositoryContent: getRepositoryContent,
+        addUserToRepository: addUserToRepository
+    });
+    var serverUri = serverIp + ':' + serverPort;
+    _server.bind(serverUri, grpc.ServerCredentials.createInsecure());
+    winston.log('info', 'RPC init succesful on: ' + serverUri);
+};
+
+exports.start = function () {
+    _server.start();
+    winston.log('info', 'RPC server started');
+};
+
+/**
+ * Implements the GetFile RPC method.
+ */
+function getRepositories(call, callback) {
+    winston.log('info', 'getRepositories rpc method request: ' + JSON.stringify(call.request));
+    if (!call.request.auth) {
+        _error('getRepositories', 'missing parameter', callback);
+    } else {
+        var auth = call.request.auth;
+        var token;
+        if(auth.type === 'BASIC'){
+            token = _basicAuthEncryption(call.request.auth.token)
+        } else {
+            token = auth.token;
+        }
+        console.log(token);
+        connector.getRepositories(token, function (err, repos) {
+            if (err) {
+                winston.log('error', 'error performing getRepositories: ', err);
+                return callback(null, {err: err.message});
+            }
+            winston.log('info', 'succesfully performed getRepositories rpc method');
+            return callback(null, {repos: JSON.stringify(repos)});
+        });
+    }
+}
+
+
+/**
+ * Implements the GetFileTree RPC method.
+ */
+function getRepositoryContent(call, callback) {
+    winston.log('info', 'getRepositoryContent rpc method request: ' + JSON.stringify(call.request));
+    if (!call.request.auth || !call.request.repositoryName || !call.request.path) {
+        _error('getRepositoryContent', 'missing parameter', callback);
+    } else {
+        var auth = call.request.auth;
+        var token;
+        if(auth.type === 'BASIC'){
+            token = _basicAuthEncryption(call.request.auth.token)
+        } else {
+            token = auth.token;
+        }
+        console.log(token);
+        connector.getRepoFiles(token, call.request.repositoryName, call.request.path, function (err, dirs) {
+            if (err) {
+                winston.log('error', 'error performing getRepositoryContent: ', err);
+                return callback(null, {err: err.message});
+            }
+            winston.log('info', 'succesfully performed getRepositoryContent rpc method', dirs);
+            return callback(null, {dirs: JSON.stringify(dirs)});
+        });
+    }
+}
+
+/**
+ * Implements the UploadFile RPC method.
+ */
+function addUserToRepository(call, callback) {
+    winston.log('info', 'addUserToRepository rpc method request');
+    if (!call.request.auth || !call.request.repositoryName || !call.request.usernameToAdd) {
+        _error('addUserToRepository', 'missing parameter', callback);
+    } else {
+        var auth = call.request.auth;
+        var token;
+        if(auth.type === 'BASIC'){
+            token = _basicAuthEncryption(call.request.auth.token)
+        } else {
+            token = auth.token;
+        }
+        console.log(token);
+        connector.addUserToRepo(token, call.request.repositoryName, call.request.usernameToAdd, function (err, status) {
+            if (err) {
+                winston.log('error', 'error performing addUserToRepository: ', err);
+                return callback(null, {err: err.message});
+            }
+            winston.log('info', 'succesfully performed addUserToRepository rpc method');
+            return callback(null, {status: status});
+        });
+    }
+}
+
+/**
+ * Converts a base64 encoded basic auth token to username and password
+ * @param token
+ * @returns {{username: *, password: *}}
+ * @private
+ */
+function _basicAuthEncryption(token) {
+    var withoutBasic = token.substr(6);
+    var readableString = new Buffer(withoutBasic, 'base64').toString();
+    var userPasswordArray = readableString.split(':');
+    return {username: userPasswordArray[0], password: userPasswordArray[1]};
+}
+
+
+function _error(functionName, errorMessage, callback) {
+    var error = new Error(errorMessage);
+    winston.log('error', 'error performing rpc method ' + functionName + ' ' + error);
+    return callback(null, {err: error.message});
+}
